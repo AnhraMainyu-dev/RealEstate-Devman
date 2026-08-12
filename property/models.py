6cf535396455e00f9e116fde290cfac9fe57e2bb
AnhraMainyu <maximheilelov@gmail.com>
2026-08-12 17:09:55 +0300

    setup Flat changelist page

--- property/models.py
+++ property/models.py
@@ -9,7 +9,7 @@ class Flat(models.Model):
         'Когда создано объявление',
         default=timezone.now,
         db_index=True)
-    new_building = models.BooleanField('Новостройка', default=False)
+    new_building = models.BooleanField('Новостройка', default=None, null=True, blank=True)
 
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
